a6972d421c98a3aecba22d534dc4528c7e8219e8
ion.cojocari <cojocari.1998@gmail.com>
2020-08-07 08:22:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const app = express();
+const homeRoutes = require('./routes/home');
+const addRoutes = require('./routes/add');
+const coursesRoutes = require('./routes/courses');
+const cardRoutes = require('./routes/card');
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
+app.use('/', homeRoutes);
+app.use('/add', addRoutes);
+app.use('/courses', coursesRoutes);
+app.use('/card', cardRoutes);
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+    console.log(`Server runing on port ${PORT}`);
+});
